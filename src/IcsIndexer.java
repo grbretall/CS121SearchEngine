91db8d5ad41e429cbd6f39e6a0bfa35bf3245ba5
gbretall <grbretall@gmail.com>
2016-02-16 16:56:36 -0800

    Added some basic functionality that parses one of the files and then strips the tags out of it

--- src/IcsIndexer.java
+++ src/IcsIndexer.java
@@ -1,8 +1,38 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import org.jsoup.Jsoup;
+
 
 public class IcsIndexer 
 {
-	public static void createIndex()
+	public static void createIndex() throws FileNotFoundException
 	{
-		
+		Scanner scan = new Scanner(new File("bbidyuk_html_files/Html/access.ics.uci.educontact.html"));
+		String textFile = "";
+		while(scan.hasNextLine())
+		{
+			textFile += scan.nextLine();
+		}
+		System.out.println(textFile);
+		textFile = htmlToText(textFile);
+		System.out.println(textFile);
+	}
+	
+	public static String htmlToText(String html)
+	{
+		return Jsoup.parse(html).text();
+	}
+	
+	public static void main(String[] args)
+	{
+		try {
+			createIndex();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
+
