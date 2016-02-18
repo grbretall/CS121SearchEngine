d89dc9bdb24ab1fe0f229be1c8f643fe4c907bb0
gbretall <grbretall@gmail.com>
2016-02-18 14:34:11 -0800

    Added the json parsing library and it almost works, just needs some tweaking

--- src/IcsIndexer.java
+++ src/IcsIndexer.java
@@ -1,20 +1,37 @@
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 import org.jsoup.Jsoup;
-
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class IcsIndexer 
 {
-	public static void createIndex() throws FileNotFoundException
+	public static void createIndex() throws FileNotFoundException, ParseException
 	{
-		Scanner scan = new Scanner(new File("bbidyuk_html_files/Html/access.ics.uci.educontact.html"));
+		JSONParser parser = new JSONParser();
+		
+			JSONObject arr;
+			try {
+				arr = (JSONObject)parser.parse(new InputStreamReader(new FileInputStream("bbidyuk_html_files/html_files.json")));
+				System.out.println(arr.get("0"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		Scanner htmlScanner = new Scanner(new File("bbidyuk_html_files/Html/access.ics.uci.educontact.html"));
 		String textFile = "";
-		while(scan.hasNextLine())
+		while(htmlScanner.hasNextLine())
 		{
-			textFile += scan.nextLine();
+			textFile += htmlScanner.nextLine();
 		}
+		htmlScanner.close();
 		System.out.println(textFile);
 		textFile = htmlToText(textFile);
 		System.out.println(textFile);
@@ -25,11 +42,11 @@ public class IcsIndexer
 		return Jsoup.parse(html).text();
 	}
 	
-	public static void main(String[] args)
+	public static void main(String[] args) throws ParseException
 	{
 		try {
 			createIndex();
-		} catch (FileNotFoundException e) {
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
